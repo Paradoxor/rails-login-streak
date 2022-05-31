b05a3a7db85520b30ca0336763c55078367c32e2
Tobias Knaus <2324623+Paradoxor@users.noreply.github.com>
2022-05-31 09:35:28 +0200

    add inital gem setup

---
+++ lib/login/streak.rb
@@ -0,0 +1,8 @@
+require "login/streak/version"
+
+module Login
+  module Streak
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/login/streak/version.rb
@@ -0,0 +1,5 @@
+module Login
+  module Streak
+    VERSION = "0.1.0"
+  end
+end
